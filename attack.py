4b460c69770257b0d3d14f96465d8b82672652c0
tank351 <hossam_one@hotmail.com>
2022-04-14 23:29:40 +0300

    "fixed if channels were'nt in args again"

--- attack.py
+++ attack.py
@@ -1,13 +1,11 @@
 import argparse
 import fcntl
 import os
-import sys
-
+import getmac
+from getmac import get_mac_address
 from progress.bar import IncrementalBar, ShadyBar, PixelBar, Bar, FillingSquaresBar, ChargingBar, FillingCirclesBar
 from progress.spinner import Spinner, MoonSpinner, PixelSpinner, PieSpinner, LineSpinner
 from scapy.all import *
-from threading import Thread
-import pandas
 import time
 import netifaces
 from scapy.layers.dot11 import Dot11, Dot11Elt, RadioTap, Dot11Deauth, Dot11Beacon, Dot11ProbeResp
@@ -51,22 +49,11 @@ def arg_parse():
                         help='choose the channel range of which to sniff on')
 
     args = vars(parser.parse_args())
-    if int(args['channels'][0]) not in range(1, 14) and int(args['channels'][1]) not in range(1, 14):
-        print("Invalid channels. Channels should be between 1 and 14")
-        sys.exit()
-
     if args['interface'] not in netifaces.interfaces():
         print('Interface not found.')
         sys.exit()
-    if args['channels'][0] and args['channels'][1]:
-        if int(args['channels'][0]) > int(args['channels'][1]):
-            ch1 = args['channels'][1]
-            ch2 = args['channels'][0]
-        else:
-            ch1 = args['channels'][0]
-            ch2 = args['channels'][1]
 
-    return args['interface'], ch1, ch2
+    return args['interface'], args['channels']
 
 
 # switch interface to monitor mode
@@ -105,10 +92,8 @@ def handler(pkt):
 
 # mac address of the interface on monitor mode
 def mon_mac(mon_iface):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    info = fcntl.ioctl(s.fileno(), 0x8927, struct.pack('256s', mon_iface[:15]))
-    mac = ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
-    return mac
+    mon = get_mac_address(interface=mon_iface)
+    return mon
 
 
 # ignore broadcasts from APs
@@ -122,24 +107,34 @@ def noise_filter(addr1, addr2):
             return True
 
 
-def sniffer(iface, ch1='1', ch2='14'):
-    i = int(ch1)
-    j = int(ch2)
+def sniffer(iface, ch):
+    if ch:
+        if int(ch[0]) not in range(1, 14) and int(ch[1]) not in range(1, 14):
+            print("Invalid channels. Channels should be between 1 and 14")
+            sys.exit()
+        if int(ch[0]) > int(ch[1]):
+            from_ch = int(ch[1])
+            to_ch = int(ch[0])
+        else:
+            from_ch = int(ch[0])
+            to_ch = int(ch[1])
+    else:
+        from_ch = 1
+        to_ch = 14
     timeout = time.time() + 60  # a minute  from now
     while True:
-        os.system("iwconfig " + iface + " channel " + str(i))  # switch channel
-        i = i % j + 1
+        os.system("iwconfig " + iface + " channel " + str(from_ch))  # switch channel
+        from_ch = from_ch % to_ch + 1
         sniff(prn=handler, iface=iface, timeout=1, monitor=True)
         if time.time() > timeout:
             break
 
 
 def output():
-    print('ACCESS POINTS:')
     dash = '-' * 60
     global AP
     print(dash)
-    print('{:<20s}{:>10s}{:>20s}'.format('ESSID', 'CH', 'BSSID'))
+    print('{:<20s}{:>10s}{:>25s}'.format('ESSID', 'CH', 'Access Points'))
     print(dash)
     for i in AP:
         ssid = AP[i]['ssid']
@@ -157,21 +152,20 @@ def progressbar():
 
 
 if __name__ == "__main__":
-    interface, from_ch, to_ch = arg_parse()
+    interface, channel = arg_parse()
     print(presentation)
     progress()
     os.system('clear')
     time.sleep(1)
     monitor_mode(interface)
     mac = mon_mac(interface)
-    print(mac)
     print("[+]Interface switched to monitor mode")
     time.sleep(1)
     print("[+]Sniffing packets this will take a minute")
     time.sleep(1)
     progbar = Thread(target=progressbar)
     progbar.start()
-    sniffer(interface, from_ch, to_ch)
+    sniffer(interface, channel)
     time.sleep(1)
     os.system('clear')
     output()
