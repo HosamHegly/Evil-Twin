c54725ecdc52692d484ab42ad0ea320fdaf9c6db
tank351 <hossam_one@hotmail.com>
2022-05-11 23:34:50 +0300

    "improved defence code"

--- defence.py
+++ defence.py
@@ -1,25 +1,28 @@
 import signal
 
 from scapy.all import *
-from scapy.layers.dot11 import Dot11, Dot11Deauth, Dot11Beacon
+from scapy.layers.dot11 import Dot11, Dot11Deauth, Dot11Beacon, RadioTap
 
-stations = dict()
+AP = dict()
 mac = ''
-ssid = ''
+essid = ''
+ch = 1
+stop_thread = False
+evil = ''
 
 
 def handler(pkt):
-    global stations
+    global AP
     global mac
     if pkt.haslayer(Dot11Deauth) and pkt.addr2 is not None and pkt.addr3 is not None:
-        if pkt.addr1 not in stations:
-            stations[pkt.addr1] = 0
+        if pkt.addr3 not in AP:
+            AP[pkt.addr3] = 0
 
         else:
-            stations[pkt.addr1] += 1
-            if stations[pkt.addr1] > 30:
-                print("[+] detected possible deauth attack on " + pkt.addr1)
-                mac = pkt.addr2
+            AP[pkt.addr3] += 1
+            if AP[pkt.addr3] > 30:
+                print("[+] detected possible deauth attack on " + pkt.addr3)
+                mac = pkt.addr3
                 return True
 
 
@@ -49,11 +52,15 @@ def ap_handler(pkt):
 
 
 def change_channel(iface):
-    ch = 1
-    os.system("iwconfig " + iface + " channel " + str(ch))
-    # switch channel from 1 to 14 each 0.5s
-    ch = ch % 14 + 1
-    time.sleep(0.5)
+    global stop_thread
+    while True:
+        ch = 1
+        os.system("iwconfig " + iface + " channel " + str(ch))
+        # switch channel from 1 to 14 each 0.5s
+        ch = ch % 14 + 1
+        time.sleep(0.5)
+        if stop_thread:
+            break
 
 
 def sig_handler(signum, frame):
@@ -61,20 +68,71 @@ def sig_handler(signum, frame):
     sys.exit()
 
 
+def add_ap(pkt):
+    global from_ch
+    global AP
+    global essid
+    global evil
+    ssid = pkt[Dot11Beacon].network_stats()['ssid']
+
+    bssid = pkt[Dot11].addr3.lower()  # ap mac address
+    if str(bssid).lower() == mac.lower():
+        essid = ssid
+    if essid != '' and ssid == essid and bssid != mac.lower():
+        evil = bssid
+
+    ap_channel = str(from_ch)
+    if bssid in AP:
+        return
+    else:
+        AP[bssid] = {}
+        AP[bssid]['channel'] = ap_channel
+        AP[bssid]['ESSID'] = ssid
+
+
+def sniffer(iface):
+    global ch
+    timeout = time.time() + 60  # a minute  from now
+    while True:
+        os.system("iwconfig " + iface + " channel " + str(ch))  # switch channel
+        ch = ch % 14 + 1
+        sniff(prn=handler, iface=iface, timeout=1)
+        if time.time() > timeout:
+            break
+
+
+def deauth(iface):
+    global evil
+    dot11 = Dot11(type=0, subtype=12, addr1='ff:ff:ff:ff:ff:ff', addr2=evil, addr3=evil)
+    frame = RadioTap() / dot11 / Dot11Deauth(reason=7)
+    os.system("iwconfig " + iface + " channel " + str(AP[evil]['channel']))
+    print("[+] started deauth attack...")
+    sendp(frame, iface=iface, loop=1, inter=.1, verbose=1)
+
+
 if __name__ == "__main__":
     interface = input("please enter the name of the interface you want to work with: ")
     monitor_mode(interface)
     time.sleep(1)
-    Thread(target=change_channel, args=(interface,)).start()
-    print("Beggining scan if you want to stop press ctrl-c")
+    Th = Thread(target=change_channel, args=(interface,)).start()
     time.sleep(2)
     while True:
+        print("[+]starting scan if you want to stop press ctrl-c")
+
         signal.signal(signal.SIGINT, sig_handler)
 
         print("[+] scanning for deauth attacks")
         sniff(iface=interface, stop_filter=handler)
-        print("[+] checking for an evil twin access point this will take a minute...")
-        sniff(iface=interface, prn=ap_handler, timeout=30)
+        stop_thread = True
+        Th.join()
+        print("[+] scanning APs and checking for evil twin this will take a minute...")
+        sniffer(interface)
+        if evil != '':
+            print("[+]evil twin detected", evil, " launching deauth attack on evil AP press ctrl-c to stop")
+            deauth(interface)
 
-        for i in stations:
-            stations[i] = 0
+        else:
+            print("[+] didnt detect evil twin")
+
+        for i in AP:
+            AP[i] = 0
